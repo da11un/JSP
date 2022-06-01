92722ae84474fc852b2838d040b820ac5462ee97
da11un <daeun32174@gmail.com>
2022-06-01 15:03:26 +0900

    Survey Project JSP로 구현하기

---
+++ Project/src/project_survey/BoardVo.java
@@ -0,0 +1,13 @@
+package project_survey;
+
+import java.util.Date;
+
+
+public class BoardVo {
+	private int number;
+	private String title;
+	private String content;
+	private String writer;
+	
+
+}
